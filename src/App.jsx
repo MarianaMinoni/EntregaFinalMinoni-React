a901ac7008d28150090f5cbdfb5db633fec19ec5
Mariana Minoni <marianaminoni@gmail.com>
2023-09-12 18:47:15 -0300

    actualización cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Card from './components/Cards/Card'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import NavBar from './components/Navbar/NavBar'
 
@@ -14,7 +15,19 @@ function App() {
     <div className="container">
    <NavBar/>
    <ItemListContainer greeting="Bienvenidos!!"/>
+   <div className="row">
+    <div className="col-3">
+   < Card title="Cerveza Warsteiner" />
    </div>
+   <div className="col-3">
+   < Card title="Cerveza Grolsch" />
+   </div>
+   <div className="col-3">
+   < Card title="Cerveza Amstel" />
+   </div>
+   </div>
+   </div>
+  
   )
 }
 
---
+++ src/components/Cards/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div className="card">
+        
+        <div className="card-body">
+
+            <p className="card-title"> {props.title} </p>
+
+        </div>  
+
+    </div>
+  )
+}
+
+export default Card
